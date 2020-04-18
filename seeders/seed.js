3ae7df1ce01fde7b30a087738ac4f3244addc22c
nazkin <nazar.kinash93@gmail.com>
2020-04-18 19:19:50 -0400

    predeploy

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,7 +2,7 @@ let mongoose = require("mongoose");
 let Workout = require("../models/Workout");
 let Exercise = require("../models/Exercise");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-
+require('dotenv').config();
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/workout", {
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost:27017/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
