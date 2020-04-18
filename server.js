a67a81b90e85b4f82e323f6b637c0f53b656430b
nazkin <nazar.kinash93@gmail.com>
2020-04-18 19:22:18 -0400

    pre-deploy tail error fix

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
-
-const app = express();
 require('dotenv').config();
+const app = express();
+
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
